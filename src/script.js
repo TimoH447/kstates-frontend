bd22e907d81f584983f7adc828a0948a92e5015c
TimoH447 <timo.hauser66@gmail.com>
2025-06-23 20:26:40 +0200

    add pd notation and jones polynomial to the result page

--- src/script.js
+++ src/script.js
@@ -88,6 +88,7 @@ async function loadResults() {
     <p><strong>Number of States:</strong> ${data.number_of_states}</p>
     <p><strong>F-Polynomial:</strong> ${data.f_polynomial}</p>
     <p><strong>Alexander Polynomial:</strong> ${data.alexander_polynomial}</p>
+    <p><strong>Jones Polynomial:</strong> ${data.jones_polynomial}</p>
     <p><strong>Kauffman Bracket:</strong> ${data.kauffman_bracket}</p>
     <p><strong>Minimal State:</strong> ${data.minimal_state}</p>
     <p><strong>Maximal State:</strong> ${data.maximal_state}</p>
